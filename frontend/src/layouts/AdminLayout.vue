1bd3903c4b6c1326671e2e9f44044b97f2bb6d7c
Suraj Kumar <154964701+surajskrv@users.noreply.github.com>
2026-01-15 02:29:05 +0530

    Refactor AdminLayout.vue structure and styles

--- frontend/src/layouts/AdminLayout.vue
+++ frontend/src/layouts/AdminLayout.vue
@@ -22,7 +22,6 @@
             height="32"
             viewBox="0 0 250 40"
             xmlns="http://www.w3.org/2000/svg"
-            class="d-none d-sm-block"
           >
             <text
               x="0"
@@ -199,4 +198,5 @@ export default {
 .bg-light-subtle {
   background-color: #f8f9fa; /* Slightly off-white page background */
 }
+
 </style>
